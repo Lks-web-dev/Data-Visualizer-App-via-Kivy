0c1aa520c4cf9bd398d16ca419fc811965a362fc
Luka Sainsily <luka.sainsily@gmail.com>
2023-05-28 12:06:35 +0200

    Implémentation des "CheckBox" : On lie la checkbox avec les fichier uploadés Bind de la checkbox avec la fonction binder() Ajout des fichiers images : - checkbox_nor.png - checkbox_tic.png

--- visualizer.py
+++ visualizer.py
@@ -1,14 +1,19 @@
 
 from kivy.app import App
 from kivy.clock import Clock
+
 from kivy.core.window.window_x11 import Config
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.checkbox import CheckBox
 from kivy.uix.image import Image
-
+from kivy.uix.label import Label
+#------------------------
+import pandas as pd
 from plyer import filechooser
+from matplotlib import pyplot as plt
 
-Config.set('graphics', 'width', '1000')
+Config.set('graphics', 'width', '1200')
 Config.set('graphics', 'height', '600')
 Config.write()
 
@@ -18,12 +23,49 @@ class Image_btn(ButtonBehavior, Image):
 
 
 class Interface(BoxLayout):
+    obj_dict = {}
+    colomn_dict = {}
+    files = ""
+    # def __init__(self, **kwargs):
+    #     super().__init__(**kwargs)
+
+    def binder(self, checkbox, value):
+        if value:
+            print(self.obj_dict[checkbox], "Ajouté")
+        else:
+            print(self.obj_dict[checkbox], "retiré")
+
     def upload(self, dt):
+
         # files retourne le répertoire
-        files = filechooser.open_file(title="Choose excel files", filter=[["*.xlsx"]], multiple=True)
-        print(files)
+        self.files = filechooser.open_file(title="Choose excel files", filter=[["*.xlsx"]], multiple=True)
+        for file in self.files:
+            file_name = file.split('/')[-1]
+            print(file_name)
+            box = BoxLayout(size_hint_y=None, height=20, padding=[30, 0, 0, 0])
+            checkbox = CheckBox(size_hint_x=.25, background_checkbox_normal="checkbox_nor.png", background_checkbox_down="checkbox_tic.png")
+            checkbox.bind(active=self.binder)
+            label = Label(text=f'[color=#3f51b5]{file_name}[/color]', markup=True)
+            box.add_widget(checkbox)
+            box.add_widget(label)
+            self.ids.file_placeholder.add_widget(box)
+            self.obj_dict[checkbox] = file_name
+        columns = pd.read_excel(self.files[0]).columns.values.tolist()
+        for column in columns:
+            box = BoxLayout(size_hint_y=None, height=20, padding=[30, 0, 0, 0])
+            checkbox = CheckBox(size_hint_x=.25, background_checkbox_normal="checkbox_nor.png",
+                                background_checkbox_down="checkbox_tic.png")
+            label = Label(text=f'[color=#3f51b5]{column}[/color]', markup=True, text_size=(170, None))
+            box.add_widget(checkbox)
+            box.add_widget(label)
+            self.colomn_dict[checkbox] = column
+            self.ids.property_placeholder.add_widget(box)
+        
         self.ids.upload_btn.source = "Drag.png"
 
+    def update(self):
+        # Ici on va définir le nombre de graphique
+        file_len = len(self.files)
     def upload_menu(self):
         self.ids.upload_btn.source = "Drop.png"
         Clock.schedule_once(self.upload)
